c9621c014a2efab0143e2742632cade81b77a308
DifanaDAP <148295120+DifanaDAP@users.noreply.github.com>
2025-03-27 12:10:12 +0700

    new file:   Day 15/Day15.py

---
+++ Day 15/Day15.py
@@ -0,0 +1,50 @@
+# Day 15 Debugging & logging dalam pyhton
+
+# pergertian Debugging & logging
+# debugging adalah proses menentukan dan memperbaiki kesalahan(bug) dalam kode program
+#logging adalah mekanisme untuk mencatat informasi selama eksekusi program guna memantau dan menganalisa perilaku program
+
+import logging
+
+# 1. mengaktifkan logging
+logging.basicConfig(level=logging.DEBUG, format='%(arstime)s - %(levelname)s - %(message)s')
+
+# 2. contoh debuging dengan print
+print("memulai program...")
+
+def bagi(a, b):
+    print(f"membagi {a} dengan {b}")
+    return a / b
+
+try:
+    hasil = bagi(10, 0)
+    print("hasil", hasil)
+except ZeroDivisionError as e:
+    print("Terjadi Kesalahan", e)
+
+# 3. Debugging dengan logging
+def bagi_logging(a, b):
+    logging.debug(f"Membagi {a} dengan {b}")
+    try:
+        hasil = a / b
+        logging.info(f"Hasil pembagian: {hasil}")
+        return hasil
+    except ZeroDivisionError:
+        logging.error("Terjadi kesalahan: Pembagian dengan nol")
+        return None
+    
+bagi_logging(10, 2)
+bagi_logging(10, 0)
+
+# 4. Menggunakan Breakpoint untuk debugging
+import pdb
+
+def hitung_faktor(x):
+    pdb.set_trace()  # Breakpoint
+    faktor = []
+    for i in range(1, x + 1):
+        if x % i == 0:
+            faktor.append(i)
+    return faktor
+
+print("Faktor dari 12:", hitung_faktor(12))
